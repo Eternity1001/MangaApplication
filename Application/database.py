6b0ee9d52d4f5778bdf0e7802504d79f6fecc525
Eternity1001 <mmirhossain01@gmail.com>
2025-01-06 13:10:19 -0500

    added the database file to hold all our data based function

---
+++ Application/database.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+def create_database():
+    
+    con = sqlite3.connect("manga.db")
+    cursor = con.cursor()
+    cursor.execute("""
+                   
+                   
+                   
+                   
+                   
+                   """)
+    
+    
+def get_con():
+    ""
