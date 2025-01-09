299decc01020272556a90a8a806e97f4192378a0
Eternity1001 <mmirhossain01@gmail.com>
2025-01-09 10:12:23 -0500

    added some db support time to work on gui

--- Application/database.py
+++ Application/database.py
@@ -3,7 +3,7 @@ import sqlite3
 
 def create_database():
     
-    con = sqlite3.connect("../.venv/manga.db")
+    con = sqlite3.connect("../.venv/Manga.db")
     cursor = con.cursor()
     cursor.execute("""CREATE TABLE Manga (
         Title varchar(500),
@@ -15,6 +15,20 @@ def create_database():
     )""")
     
     
-def get_con():
-    ""
+def get_con() -> sqlite3.Connection:
+    con = sqlite3.connect("Manga.db")
+    return con
 
+
+def commit(con: sqlite3.Connection) -> None:
+    con.commit()
+
+def insert(title: str, genre: str, thumpnail: str, main: str, chapter: str,  con: sqlite3.Connection, read:str = "0"):
+    
+    cursor = con.cursor()
+    data = (title, genre, thumpnail, main, chapter, read)
+    cursor.execute("""INSERT OR REPLACE INTO Manga VALUES (?, ?, ?, ?, ?, ?)""", data)
+    commit(con)
+    
+    
+    
